97a3b83a5fec3a19889fb28a00d362f666fcddff
pencilCool <yhtang_xt@163.com>
2017-05-24 11:02:31 +0800

    init

---
+++ Tutorial.playground/Pages/Making it Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: Back to [The C API](@previous)
+
+import Foundation
+import SQLite
+import XCPlayground
+
+destroyPart2Database()
+
+//: # Making it Swift
+
+
+//: ## Errors
+
+
+//: ## The Database Connection
+
+
+//: ## Preparing Statements
+
+
+//: ## Create Table
+
+
+//: ## Insert Row
+
+
+//: ## Read
---
+++ Tutorial.playground/Pages/The C API.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+import SQLite
+import XCPlayground
+import Foundation
+
+destroyPart1Database()
+
+/*: 
+
+# Getting Started
+
+The first thing to do is set your playground to run manually rather than automatically. This will help ensure that your SQL commands run when you intend them to. At the bottom of the playground click and hold the Play button until the dropdown menu appears. Choose "Manually Run". 
+
+You will also notice a `destroyPart1Database()` call at the top of this page. You can safely ignore this, the database file used is destroyed each time the playground is run to ensure all statements execute successfully as you iterate through the tutorial.
+
+Secondly, this Playground will need to write SQLite database files to your file system. Create the directory `~/Documents/Shared Playground Data/SQLiteTutorial` by running the following command in Terminal.
+
+`mkdir -p ~/Documents/Shared\ Playground\ Data/SQLiteTutorial`
+
+*/
+
+//: ## Open a Connection
+ 
+
+//: ## Create a Table
+
+
+//: ## Insert a Contact
+
+
+//: ## Querying
+
+
+//: ## Update
+
+
+//: ## Delete
+
+
+//: ## Errors
+
+
+//: ## Close the database connection
+
+
+//: Continue to [Making It Swift](@next)
